e81fdbf2830d6df7e1b1004248c95d8207cbb6ef
Kale Leach <kleach1212@gmail.com>
2022-03-02 13:51:36 -0800

    Commit 9: Creating Add Toy Form

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-// const gameButton = document.querySelector("#add-game")
 
-// function addGameForm(){
-//     gameButton.addEventListener('cicke',(e) => {
-//         console.log("hi")
-//     })
-// }
+const gameButton = document.querySelector("#add-game")
+const addGameForm = document.querySelector('#game-form')
+
+gameButton.addEventListener('click',(e) => {
+    addGameForm.innerHTML = `
+        <ul>
+        <label class = "center"> Name </label><input type="text"></lable>
+        <label class = "center"> IMG URL</label><input type="text"></lable>
+        <label class = "center"> Price</label><input type="text"></lable>
+        <label class = "center"> Systems</label><input type="text"></lable>
+        <label class = "center"> Description</label><input type="text"></lable>
+        <input id ='submit-game'class = "center" type="submit" value = 'Get Rated'>
+      </ul>`
+    })
+
 });
