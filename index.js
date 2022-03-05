10b6bf0c23de9e50cc123d4afd272447f84caf11
Kale Leach <kleach1212@gmail.com>
2022-03-05 13:40:38 -0800

    Commit 12: Successful Creation of Gamecard

--- index.js
+++ index.js
@@ -3,40 +3,44 @@ document.addEventListener('DOMContentLoaded', (event) => {
 const gameButton = document.querySelector("#add-game")
 const addGameForm = document.querySelector('#game-form')
 const submitGame = document.querySelector('#submit-game')
+console.log(submitGame)
 
 gameButton.addEventListener('click',(e) => {
     addGameForm.innerHTML = `
+    <form id = 'gameform'>
         <ul>
-        <label id = 'game-name' class = "center"> Name </label><input class = 'textbox' type="text" ></lable>
-        <label id = 'game-img' class = "center"> IMG URL</label><input class = 'textbox' type="text" ></lable>
-        <label id = 'game-price' class = "center"> Price</label><input class = 'textbox' type="text" ></lable>
-        <label id = 'game-system' class = "center"> Systems</label><input type="text"></lable>
-        <label id = 'game-description' class = "center"> Description</label><input type="text"></lable>
+        <label class = "center"> Name </label><input id = 'game_name' class = 'textbox' type="text" ></lable>
+        <label class = "center"> IMG URL</label><input id = 'game_img' class = 'textbox' type="text" ></lable>
+        <label class = "center"> Price</label><input id = 'game_price' class = 'textbox' type="text" ></lable>
+        <label class = "center"> Systems</label><input id = 'game_system' class = 'textbox' type="text"></lable>
+        <label class = "center"> Description</label><input id = 'game_description' type="text"></lable>
         <input id ='submit-game' class = "center" type="submit" value = 'Get Rated'>
-      </ul>`
-
-      // const submitGame = document.querySelector('#submit-game')
-
-      // // submitGame.addEventListener('click',(e) => {
-      // //   console.log('hi')
-      // // })
-    })
-
-    function handleGameSubmit(e){
-      e.preventDefault()
-      let game = {
-        name:e.target.name.value,
-        imgUrl: e.target.img_url.value,
-        description:e.target.description.value,
+      </ul>
+      </form id = gameform>`
         
+         const gameform = document.querySelector('#gameform')
 
+        //  Created the handlesubmit function within adding a game funtion so it could take be in the function scope. Without being in this function 
+         gameform.addEventListener('submit', (e) => {
+            e.preventDefault(e)
+            //debugger; 
+          let game = {
+            name: e.target.game_name.value,
+            imgurl: e.target.game_img.value,
+            price:e.target.game_price.value,
+            systems:e.target.game_system.value,
+            description:e.target.game_description.value,
+          }
+          renderGame(game)
+         //debugger
+    })
+})
 
-      }
-    }
 
     function renderGame(game){
-      let gameCard = document.createElement('li')
-      gameCard.className = 'gamecard'
+debugger
+      let gameCard = document.createElement('div')
+      gameCard.className = 'card'
       gameCard.innerHTML = `
       <h3> ${game.name} </h3>
       <img src = ${game.imgurl} />
@@ -45,6 +49,9 @@ gameButton.addEventListener('click',(e) => {
       <p> ${game.price}</p>
       <button> 'Must Play' </button> <button> 'Return' </button><button> 'Retrun it'</button>
       `
+debugger
+      document.body.appendChild(gameCard)
+debugger
     }
 
     
