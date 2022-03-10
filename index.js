5cf26dbd3b4743667bd4cb39d5b03898d9713eca
Kale Leach <kleach1212@gmail.com>
2022-03-09 18:35:18 -0800

    Commit 25: Semi-Final Commit

--- index.js
+++ index.js
@@ -9,37 +9,37 @@ document.addEventListener('DOMContentLoaded', (event) => {
   
   
   function createGameForm(e){
-  
     addGameForm.innerHTML = `
-        <ul>
-        <form id = 'gameform'>
-        <label class = "center"> Name </label><input id = 'game_name' class = 'textbox' type="text" ></lable>
-        <label class = "center"> IMG URL</label><input id = 'game_img' class = 'textbox' type="text" ></lable>
-        <label class = "center"> Price</label><input id = 'game_price' class = 'textbox' type="text" ></lable>
-        <label class = "center"> Systems</label><input id = 'game_system' class = 'textbox' type="text"></lable>
-        <label class = "center"> Description</label><input id = 'game_description' type="text"></lable>
-        <input id ='submit-game' class = 'center' type="submit" value = 'Get Rated'/>
-        </form id = gameform>
-        <button id = 'back_btn'> Hide </button>
-        </ul>`
+      <ul>
+      <form id = 'gameform'>
+      <label class = "center"> Name </label><input id = 'game_name' class = 'textbox' type="text" ></lable>
+      <label class = "center"> IMG URL</label><input id = 'game_img' class = 'textbox' type="text" ></lable>
+      <label class = "center"> Price</label><input id = 'game_price' class = 'textbox' type="text" ></lable>
+      <label class = "center"> Systems</label><input id = 'game_system' class = 'textbox' type="text"></lable>
+      <label class = "center"> Description</label><input id = 'game_description' type="text"></lable>
+      <input id ='submit-game' class = 'center' type="submit" value = 'Get Rated'/>
+      </form id = gameform>
+      <button id = 'back_btn'> Hide </button>
+      </ul>`
           
-        const gameform = document.querySelector('#gameform')
+      const gameform = document.querySelector('#gameform')
         //console.log(gameform)
         //  Created the handlesubmit function within adding a game funtion so it could take be in the function scope. Without being in this function 
-        gameform.addEventListener('submit', (e) => {
-          e.preventDefault() 
-          let game = {
-            name: e.target.game_name.value,
-            imgurl: e.target.game_img.value,
-            price:e.target.game_price.value,
-            systems:e.target.game_system.value,
-            description:e.target.game_description.value,
-            must_play:0,
-            play:0,
-            return:0
-            }
-            renderGame(game)
-            createAGame(game)
+      gameform.addEventListener('submit', (e) => {
+        e.preventDefault() 
+        let game = {
+          name: e.target.game_name.value,
+          imgurl: e.target.game_img.value,
+          price:e.target.game_price.value,
+          systems:e.target.game_system.value,
+          description:e.target.game_description.value,
+          must_play:0,
+          play:0,
+          return:0
+          }
+          renderGame(game)
+          createAGame(game)
+          hideGame()
         })
         const backButton = document.querySelector('#back_btn')
         backButton.addEventListener('click', hideGame)
@@ -65,59 +65,62 @@ document.addEventListener('DOMContentLoaded', (event) => {
       frontGameCard.innerHTML = frontHTML
   
         contClass.appendChild(frontGameCard)
-      function createCardButton(){
-        let cardButton = document.getElementById(`btn_${game.id}`)
-        const addButton = cardButton.addEventListener('click', backCard)}
+
+  function createCardButton(){
+    let cardButton = document.getElementById(`btn_${game.id}`)
+    cardButton.addEventListener('click', backCard)}
       createCardButton()
   
-       function backCard(){
-         frontGameCard.innerHTML = 
-          `<div class = "container">
-          <div class = 'column'>
-          <h2> ${game.name} </h2>
-          <p> ${game.description} </p>
-          <div id = 'sys_price' class = 'grid'>
-          <p> System </p>
-          <p> Price </p>
-          <p> ${game.systems}</p>
-          <p> $${game.price}</p>
-          </div>
-          <div class = 'grid'>
-          <p> <span id = 'num-of-must'> ${game.must_play} </span></p> 
-          <p> <span id = 'num-of-play'> ${game.play} </span></p>
-          <p> <span id = 'num-of-return'> ${game.return} </span></p> 
-          <button class = 'Must_Button'> Must Play </button> 
-          <button class = 'Play_Button'> Play </button> 
-          <button class = 'Return_Button'> Return </button>
-          </div>
-          </div>
-          <button class = back_btn> Back </button>
-          </div">` 
+
+
+  function backCard(){
+    frontGameCard.innerHTML = 
+      `<div class = "container">
+      <div class = 'column'>
+      <h2> ${game.name} </h2>
+      <p> ${game.description} </p>
+      <div id = 'sys_price' class = 'grid'>
+      <p> System </p>
+      <p> Price </p>
+      <p> ${game.systems}</p>
+      <p> $${game.price}</p>
+      </div>
+      <div class = 'grid'>
+      <p> <span id = 'num-of-must'> ${game.must_play} </span></p> 
+      <p> <span id = 'num-of-play'> ${game.play} </span></p>
+      <p> <span id = 'num-of-return'> ${game.return} </span></p> 
+      <button class = 'Must_Button'> Must Play </button> 
+      <button class = 'Play_Button'> Play </button> 
+      <button class = 'Return_Button'> Return </button>
+      </div>
+      </div>
+      <button class = back_btn> Back </button>
+      </div">` 
   
-          frontGameCard.querySelector('.Must_Button').addEventListener('click', (e) =>{
-             game.must_play += 1
-              frontGameCard.querySelector('#num-of-must').textContent = game.must_play
-             updateGameLike(game)
-           })
+        frontGameCard.querySelector('.Must_Button').addEventListener('click', (e) =>{
+          game.must_play += 1
+          frontGameCard.querySelector('#num-of-must').textContent = game.must_play
+          updateGameLike(game)
+        })
   
-           frontGameCard.querySelector('.Play_Button').addEventListener('click', (e) =>{
-            game.play += 1
-            frontGameCard.querySelector('#num-of-play').textContent = game.play 
-            updateGameLike(game)
-          })
+        frontGameCard.querySelector('.Play_Button').addEventListener('click', (e) =>{
+          game.play += 1
+          frontGameCard.querySelector('#num-of-play').textContent = game.play 
+          updateGameLike(game)
+        })
   
-          frontGameCard.querySelector('.Return_Button').addEventListener('click', (e) =>{
-            game.return += 1
-            frontGameCard.querySelector('#num-of-return').textContent = game.return 
-            updateGameLike(game)
-          })
+        frontGameCard.querySelector('.Return_Button').addEventListener('click', (e) =>{
+          game.return += 1
+          frontGameCard.querySelector('#num-of-return').textContent = game.return 
+          updateGameLike(game)
+        })
   
-          frontGameCard.querySelector('.back_btn').addEventListener('click', (e) => {
-            frontGameCard.innerHTML = frontHTML;
-            createCardButton()
-          })
+        frontGameCard.querySelector('.back_btn').addEventListener('click', (e) => {
+          frontGameCard.innerHTML = frontHTML;
+          createCardButton()
+        })
   
-        }
+    }
   }
   
   
